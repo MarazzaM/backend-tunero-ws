a03a9b16b090572732dd303b75d77f28a25f9e9c
MarazzaM <marazza.matias@gmail.com>
2024-03-01 04:56:54 -0300

    to fix

--- src/app/totem/page.tsx
+++ src/app/totem/page.tsx
@@ -1,26 +1,28 @@
-"use client"
-import React, { useState, useEffect, useRef } from 'react';
-import createSocket from '@/lib/websocket';
+"use client";
+import React, { useState, useEffect, useRef } from "react";
+import createSocket from "@/lib/websocket";
 import { Button } from "@/components/ui/button";
-import { useReactToPrint } from 'react-to-print';
+import { useReactToPrint } from "react-to-print";
+import useSWR from "swr";
 
-const CTAButton = ({ children, onClick, color }) => {
-  let buttonColorClasses = "";
-  if (color === "green") {
-    buttonColorClasses = "bg-green-500 hover:bg-green-600";
-  } else if (color === "blue") {
-    buttonColorClasses = "bg-blue-500 hover:bg-blue-600";
-  }
+const CTAButton = ({ children, onClick, color, colorBg }) => {
+  const buttonStyle = {
+    backgroundColor: `${colorBg}`,
+    color: `${color}`,
+  };
 
   return (
     <Button
       onClick={onClick}
-      className={`text-white h-auto text-9xl font-semibold py-8 px-8 rounded-lg shadow-lg focus:outline-none focus:ring-2 focus:ring-opacity-50 transition-all duration-300 ${buttonColorClasses}`}
+      style={buttonStyle}
+      className="h-auto text-9xl font-semibold py-8 px-8 rounded-lg shadow-lg focus:outline-none focus:ring-2 focus:ring-opacity-50 transition-all duration-300"
     >
       {children}
     </Button>
   );
 };
+
+
 
 function Page() {
   const [socket, setSocket] = useState(null);
@@ -28,17 +30,37 @@ function Page() {
 
   const [messageNumber, setMessageNumber] = useState(null);
   const [messageType, setMessageType] = useState(null);
+
+  const backend = process.env.NEXT_PUBLIC_BACKEND;
+  const apiUrl = `${backend}/tipo-atencion/active`;
+  const fetcher = async (url, token) => {
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch data");
+    }
+
+    return response.json();
+  };
+  const { data, error, isLoading } = useSWR(apiUrl, fetcher);
+  console.log(data);
+
   useEffect(() => {
     const newSocket = createSocket();
     setSocket(newSocket);
 
     // Attach event listener only if socket is not null
     if (newSocket) {
-      newSocket.on('message', (message) => {
+      newSocket.on("message", (message) => {
         console.log(`Received Socket.io message` + message.number);
         setMessageSent(true);
-        setMessageNumber(message.number)
-        setMessageType(message.type)
+        setMessageNumber(message.number);
+        setMessageType(message.type);
         setTimeout(() => {
           setMessageSent(false);
         }, 3000); // Hide the success component after 3 seconds
@@ -53,28 +75,16 @@ function Page() {
     };
   }, []);
 
-  function handleNormal() {
+  function handleGenerateTicket(type: string, priority: number) {
     if (socket) {
-      // Emit the 'generateTicket' event with the hardcoded type
- const type = "hardcodedType"; // Default value or retrieve it from somewhere
-socket.emit('generateTicket', { type }, (response) => {
-  // Handle the response from the server if needed
-  console.log('Response from server:', response);
-});
-
-    }
-  }
-  
-
-  function handlePriority(){
-    if (socket) {
-      const type = "priority";
-      socket.emit("message", { type, number: "P-2" }, (response) => {
+      socket.emit("generateTicket", { type, priority }, (response) => {
+        // Handle the response from the server if needed
+        console.log("Response from server:", response);
       });
     }
   }
 
-  function closeModal(){
+  function closeModal() {
     setMessageSent(false);
   }
 
@@ -83,30 +93,50 @@ socket.emit('generateTicket', { type }, (response) => {
     const handlePrint = useReactToPrint({
       content: () => printComponentRef.current,
     });
-  
+
     setTimeout(() => {
-      handlePrint()
+      handlePrint();
     }, 100);
     return (
       <div className="fixed inset-0 flex items-center justify-center z-50 bg-white bg-opacity-50 text-black flex-col">
-        <div ref={printComponentRef} className="bg-white p-8 rounded-lg shadow-lg">
+        <div
+          ref={printComponentRef}
+          className="bg-white p-8 rounded-lg shadow-lg"
+        >
           <div className="text-center ">{number}</div>
           <div>Type: {type}</div>
         </div>
-        <Button onClick={closeModal} className='mt-4 px-6 py-3 bg-gray-500 hover:bg-gray-600 text-white font-semibold rounded-lg shadow-lg focus:outline-none focus:ring-2 focus:ring-opacity-50 transition-all duration-300'>Close</Button>
+        <Button
+          onClick={closeModal}
+          className="mt-4 px-6 py-3 bg-gray-500 hover:bg-gray-600 text-white font-semibold rounded-lg shadow-lg focus:outline-none focus:ring-2 focus:ring-opacity-50 transition-all duration-300"
+        >
+          Close
+        </Button>
       </div>
     );
   };
 
   return (
     <div className="flex flex-row gap-8 justify-center items-center flex-grow">
-      {messageSent && <SuccessComponent number={messageNumber} type={messageType} />}
-      <CTAButton onClick={handleNormal} color="green">
-        Normal
-      </CTAButton>
-      <CTAButton onClick={handlePriority} color="blue">
-        Priority
-      </CTAButton>
+      {messageSent && (
+        <SuccessComponent number={messageNumber} type={messageType} />
+      )}
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>Error: {error.message}</p>
+      ) : (
+        data.map((item) => (
+          <CTAButton
+          key={item.id}
+          onClick={() => handleGenerateTicket(item.type, item.priority)} // Handle the click event for each button
+          color={item.color}
+          colorBg={item.colorBg}
+        >
+          {item.type}
+        </CTAButton>
+        ))
+      )}
     </div>
   );
 }
